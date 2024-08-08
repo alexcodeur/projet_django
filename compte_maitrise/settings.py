d29070a88f516ec77887c295783079fd546b8d98
Alexandre <alexandre.dijos.ad@gmail.com>
2024-08-08 13:41:21 +0200

    Ajout des applications authentification et accueil

--- compte_maitrise/settings.py
+++ compte_maitrise/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Applications du projet
+    'authentification',
+    'accueil',
 ]
 
 MIDDLEWARE = [
